a8fbb9c7613578cfa9dccfd46521f26e0783760e
sb4aa4bs <bharath.softy@gmail.com>
2021-09-10 14:12:24 -0400

    Com-Coder: TwoSumLessThanK Initial Commit.

--- com-coder/src/main/java/com/apeiron/abs2/TwoSumLessThanK.java
+++ com-coder/src/main/java/com/apeiron/abs2/TwoSumLessThanK.java
@@ -3,16 +3,26 @@ package com.apeiron.abs2;
 class TwoSumLessThanK {
 
     public static void main(String[] args) {
-        int numbers[] = {1, 2, 3, 4, 5};
-        int targetVal = 10;
+        int numbers[] = {34, 23, 1, 24, 75, 33, 54, 8};
+        int targetVal = 60;
         int answer = new TwoSumLessThanK().twoSumLessThanK(numbers, targetVal);
         System.out.println("Output is " + answer);
 
     }
 
-    public int twoSumLessThanK(int[] nums, int k) {
-        int answer = -1;
-
-        return answer;
+    public int twoSumLessThanK(int[] nums, int targetVal) {
+        int correctAnswer = -1;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                sum = nums[i] + nums[j];
+                if (sum < targetVal) {
+                    if (correctAnswer < sum) {
+                        correctAnswer = sum;
+                    }
+                }
+            } // end of j
+        } // end of i
+        return correctAnswer;
     }
 }
