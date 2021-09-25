25f2ad99ee79a25938cdf04ccee7e40fc9ce21ed
sb4aa4bs <bharath.softy@gmail.com>
2021-09-25 14:56:30 -0400

    com-coder: Adding numbers using single linked list analyzijng various solutions.

--- com-coder/src/main/java/com/apeiron/abs2/AddTwoNumberUsingLinkedList.java
+++ com-coder/src/main/java/com/apeiron/abs2/AddTwoNumberUsingLinkedList.java
@@ -34,8 +34,28 @@ public class AddTwoNumberUsingLinkedList {
             ln2 = ln2.next;
         }
         ListNode result = new ListNode();
-
         return resultNode;
+    }
+
+    public static ListNode addTwoNumbersSol1(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+        //return curr.next; // returns NPE
     }
 
     public static void main(String[] args) {
@@ -43,7 +63,26 @@ public class AddTwoNumberUsingLinkedList {
         ListNode node2 = new ListNode(5, new ListNode(6, new ListNode(4, new ListNode())));
         System.out.println("Node 1: " + node1.toString());
         System.out.println("Node 2: " + node2.toString());
-        addTwoNumbers(node1, node2);
+        //addTwoNumbers(node1, node2);
+        ListNode resultNode = addTwoNumbersSol1(node1, node2);
+        System.out.println("Final Result : " + resultNode.toString());
+    }
+
+    public ListNode addTwoNumbersSol2(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode emptyNode = new ListNode(0);
+        ListNode currentNode = new ListNode(0);
+        ListNode result = currentNode;
+        while (emptyNode != l1 || emptyNode != l2) {
+            int sum = l1.val + l2.val + carry;
+            carry = sum / 10;
+            currentNode = (currentNode.next = new ListNode(sum %= 10));
+            l1 = null != l1.next ? l1.next : emptyNode;
+            l2 = null != l2.next ? l2.next : emptyNode;
+        }
+        if (carry == 1)
+            currentNode.next = new ListNode(carry);
+        return result.next;
     }
 
 }
