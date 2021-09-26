7dd4b16a0d4ab446e67822fe7241e55986ba023c
sb4aa4bs <bharath.softy@gmail.com>
2021-09-26 12:09:33 -0400

    com-coder: Daily Byte Question from Facebook to find a Palindrome String Dated 09/26/2021.

--- com-coder/src/main/java/com/apeiron/abs2/dailybyte/PalindromeFinder.java
+++ com-coder/src/main/java/com/apeiron/abs2/dailybyte/PalindromeFinder.java
@@ -1,4 +1,16 @@
 package com.apeiron.abs2.dailybyte;
 
 public class PalindromeFinder {
+    /*
+    This question is asked by Facebook. Given a string, return whether or not it forms a palindrome ignoring case and non-alphabetical characters.
+     */
+
+    String str1 = "level";
+    String Str2 = "algorithm";
+    String Str3 = "A man, a plan, a canal: Panama.";
+
+
+    public static void main(String[] args) {
+
+    }
 }
