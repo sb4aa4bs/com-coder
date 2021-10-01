6e4c081ff0074d9ea8288c1e4f953ada14df1cd8
sb4aa4bs <bharath.softy@gmail.com>
2021-10-01 17:33:03 -0400

    com-coder: Daily Byte Question from Facebook to find a Palindrome String Dated 09/26/2021, added test class for improving unit testing knowledge.

--- com-coder/src/main/java/com/apeiron/abs2/dailybyte/PalindromeFinder.java
+++ com-coder/src/main/java/com/apeiron/abs2/dailybyte/PalindromeFinder.java
@@ -5,12 +5,35 @@ public class PalindromeFinder {
     This question is asked by Facebook. Given a string, return whether or not it forms a palindrome ignoring case and non-alphabetical characters.
      */
 
-    String str1 = "level";
-    String Str2 = "algorithm";
-    String Str3 = "A man, a plan, a canal: Panama.";
+    static String str1 = "level";
+    static String str2 = "algorithm";
+    static String str3 = "A man, a plan, a canal: Panama.";
 
 
     public static void main(String[] args) {
+        System.out.println(palindrome(str3));
+    }
 
+    public static boolean palindrome(String inputStr) {
+        String trimmedString = trimNonAlphabetsFromString(str3);
+        return (trimmedString.equalsIgnoreCase(reverseString(trimmedString)) ? true : false);
+    }
+
+    public static String trimNonAlphabetsFromString(String str) {
+        StringBuilder strOut = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char chr = str.charAt(i);
+            if (chr >= 'a' && chr <= 'b' && chr >= 'A' && chr <= 'Z') {
+                strOut.append(chr);
+            }
+        }
+        return strOut.toString();
+    }
+
+    public static String reverseString(String str) {
+        for (int j = str.length() - 1; j >= 0; j--) {
+
+        }
+        return "";
     }
 }
---
+++ com-coder/src/test/java/com/apeiron/abs2/dailybyte/PalindromeFinderTest.java
@@ -0,0 +1,26 @@
+package com.apeiron.abs2.dailybyte;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PalindromeFinderTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void palindrome() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void trimNonAlphabetsFromString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void reverseString() {
+    }
+}
