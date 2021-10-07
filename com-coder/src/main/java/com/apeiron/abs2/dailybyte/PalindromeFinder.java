a60708158e4d5bf2829a44d39411af6304102e4c
sb4aa4bs <bharath.softy@gmail.com>
2021-10-06 20:00:04 -0400

    com-coder: Daily Byte Question from Facebook to find a Palindrome String Dated 09/26/2021, test case to be updated soon.

--- com-coder/src/main/java/com/apeiron/abs2/dailybyte/PalindromeFinder.java
+++ com-coder/src/main/java/com/apeiron/abs2/dailybyte/PalindromeFinder.java
@@ -5,17 +5,19 @@ public class PalindromeFinder {
     This question is asked by Facebook. Given a string, return whether or not it forms a palindrome ignoring case and non-alphabetical characters.
      */
 
-    static String str1 = "level";
-    static String str2 = "algorithm";
+    static String str1 = "level-";
+    static String str2 = "algorithm!";
     static String str3 = "A man, a plan, a canal: Panama.";
 
 
     public static void main(String[] args) {
-        System.out.println(palindrome(str3));
+        System.out.println(str1 + " is a Palindrome : " + palindrome(str1));
+        System.out.println(str2 + " is a Palindrome : " + palindrome(str2));
+        System.out.println(str3 + " is a Palindrome : " + palindrome(str3));
     }
 
     public static boolean palindrome(String inputStr) {
-        String trimmedString = trimNonAlphabetsFromString(str3);
+        String trimmedString = trimNonAlphabetsFromString(inputStr);
         return (trimmedString.equalsIgnoreCase(reverseString(trimmedString)) ? true : false);
     }
 
@@ -23,7 +25,7 @@ public class PalindromeFinder {
         StringBuilder strOut = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
             char chr = str.charAt(i);
-            if (chr >= 'a' && chr <= 'b' && chr >= 'A' && chr <= 'Z') {
+            if (chr >= 'a' && chr <= 'z' || chr >= 'A' && chr <= 'Z') {
                 strOut.append(chr);
             }
         }
@@ -31,9 +33,11 @@ public class PalindromeFinder {
     }
 
     public static String reverseString(String str) {
+        StringBuilder reverseString = new StringBuilder();
         for (int j = str.length() - 1; j >= 0; j--) {
-
+            char chr = str.charAt(j);
+            reverseString.append(chr);
         }
-        return "";
+        return reverseString.toString();
     }
 }
